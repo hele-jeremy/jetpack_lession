9fca4dd9f2291ba5b6be7c4568f44747d2418320
何乐 <hele@xgd.com>
2021-03-05 09:42:52 +0800

    初次提交

---
+++ app/src/main/java/com/jeremy/jetpack_lession/JApp.java
@@ -0,0 +1,13 @@
+package com.jeremy.jetpack_lession;
+
+import android.app.Application;
+
+public class JApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,19 @@
+object Versions {
+    const val gradleVersion = "4.0.0-beta05"
+    const val kotlin_version = "1.3.61"
+    const val androidx_appcompat = "1.2.0"
+    const val constraintlayout = "2.0.4"
+    const val compileSdkVersion = 30
+    const val buildToolsVersion = "30.0.2"
+    const val minSdkVersion = 19
+    const val targetSdkVersion = 30
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val applicationId = "com.jeremy.jetpack_lession"
+}
+
+object Deps {
+    const val kotlin_sdk = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin_version}"
+    const val appcompat = "androidx.appcompat:appcompat:${Versions.androidx_appcompat}"
+    const val constraintlayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintlayout}"
+}
